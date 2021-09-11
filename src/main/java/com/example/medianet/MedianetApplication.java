69615696b19479fa482a3a29f14e2e228c68cf45
Vishal <vishal@macbook-air.myfiosgateway.com>
2021-09-10 23:21:44 -0400

    Initial Commit from Spring Initializer with a basic controller

---
+++ src/main/java/com/example/medianet/MedianetApplication.java
@@ -0,0 +1,13 @@
+package com.example.medianet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedianetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedianetApplication.class, args);
+	}
+
+}
